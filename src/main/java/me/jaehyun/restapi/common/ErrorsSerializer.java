f2050e7e5b25ea2e2d240afc50e0a0d96a6e2b36
jaehyun <jaehyun8719@gmail.com>
2019-03-18 21:36:28 +0900

    Body for BadRequest

---
+++ src/main/java/me/jaehyun/restapi/common/ErrorsSerializer.java
@@ -0,0 +1,58 @@
+package me.jaehyun.restapi.common;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.validation.Errors;
+
+import java.io.IOException;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author : kjh
+ * github: https://github.com/jaehyun8719
+ * email: jaehyun8719@gmail.com
+ * <p>
+ * Date: 2019-03-18
+ * Copyright(©) 2019 by jaehyun.
+ */
+@JsonComponent
+public class ErrorsSerializer extends JsonSerializer<Errors> {
+    @Override
+    public void serialize(Errors errors, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeStartArray();
+        errors.getFieldErrors().stream().forEach(e -> {
+            try {
+                jsonGenerator.writeStartObject();
+                jsonGenerator.writeStringField("field", e.getField());
+                jsonGenerator.writeStringField("objectName", e.getObjectName());
+                jsonGenerator.writeStringField("code", e.getCode());
+                jsonGenerator.writeStringField("defaultMessage", e.getDefaultMessage());
+                Object rejectedValue = e.getRejectedValue();
+                if (rejectedValue != null) {
+                    jsonGenerator.writeStringField("rejectedValue", e.getRejectedValue().toString());
+                }
+
+                jsonGenerator.writeEndObject();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+
+        errors.getGlobalErrors().stream().forEach(e -> {
+            try {
+                jsonGenerator.writeStartObject();
+                jsonGenerator.writeStringField("objectName", e.getObjectName());
+                jsonGenerator.writeStringField("code", e.getCode());
+                jsonGenerator.writeStringField("defaultMessage", e.getDefaultMessage());
+                jsonGenerator.writeEndObject();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+        jsonGenerator.writeEndArray();
+    }
+}
--- src/main/java/me/jaehyun/restapi/events/EventValidator.java
+++ src/main/java/me/jaehyun/restapi/events/EventValidator.java
@@ -22,8 +22,7 @@ public class EventValidator {
 
     public void validate(EventDto eventDto, Errors errors) {
         if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() != 0) {
-            errors.rejectValue("basePrice", "wrongValue", "BasePrice is Wrong");
-            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is Wrong");
+            errors.reject("wrongPrices", "Values for Price are wrong");
         }
 
         LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
