bc1ff4769665ccdfadc6e5d6da551914c2627ebb
jaehyun <jaehyun8719@gmail.com>
2019-04-06 18:27:33 +0900

    현재 사용자 조회

---
+++ src/main/java/me/jaehyun/restapi/accounts/AccountAdapter.java
@@ -0,0 +1,40 @@
+package me.jaehyun.restapi.accounts;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author : 진실
+ * github: https://github.com/jaehyun8719
+ * email: jaehyun8719@gmail.com
+ * <p>
+ * Date: 2019-04-06
+ * Description:
+ * Copyright(©) 2019 by jaehyun.
+ **/
+public class AccountAdapter extends User {
+
+    private Account account;
+
+    public AccountAdapter(Account account) {
+        super(account.getEmail(), account.getPassword(), authorities(account.getRoles()));
+        this.account = account;
+    }
+
+    private static Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
+        return roles.stream()
+                .map(r -> new SimpleGrantedAuthority("ROLE_" + r.name()))
+                .collect(Collectors.toSet());
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+}
